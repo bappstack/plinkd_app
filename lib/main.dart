b1f8f8bf311adc1ee3c09d99243c0eead5ff0051
Ofor Emmanuel <johnebere58@gmail.com>
2019-05-30 18:34:15 +0100

    i added some things

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Plink App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue, fontFamily: poppinFont),
       home: NumberVerified(),
