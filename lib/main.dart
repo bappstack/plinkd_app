9967f262908b92bbe7a6776416a346a477ce2bc1
johnebere58 <johnebere58@gmail.com>
2019-05-30 18:44:09 +0100

    Plinkd from john

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Plink App',
+      title: 'Plink - App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue, fontFamily: poppinFont),
       home: NumberVerified(),
